035bebdfa5361967b6d82459cbd047a41f25a369
Xavier Antonio Mas <70808503+xavi956@users.noreply.github.com>
2025-01-13 09:26:42 +0100

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -20,8 +20,7 @@ struct Player {
 
 // Inicializar laberinto
 vector<vector<int>> iniciarLab(int size) {
-    vector<vector<int>> lab(size, vector<int>(size, 1)); // 1 representa muro
-    // Crear bordes
+    vector<vector<int>> lab(size, vector<int>(size, 1));
     for (int i = 0; i < size; i++) {
         lab[0][i] = lab[size - 1][i] = 1;
         lab[i][0] = lab[i][size - 1] = 1;
@@ -36,14 +35,17 @@ void conecInicFin(vector<vector<int>>& laberinto) {
 
     while (x < size - 2 || y < size - 2) {
         laberinto[y][x] = 0;
-
-        // Dirección aleatoria
-        int direction = rand() % 2; // 0 derecha, 1 abajo
+        int direction = rand() % 2;
         if (direction == 0 && x < size - 2) x++;
         else if (y < size - 2) y++;
     }
+    laberinto[size - 2][size - 2] = 0;
+}
 
-    laberinto[size - 2][size - 2] = 0; // Asegurar salida conectada
+// Dibujar una celda
+void dibujarCelda(int x, int y, int color) {
+    setfillstyle(SOLID_FILL, color);
+    bar(x * TAMAÑO_CELDA, y * TAMAÑO_CELDA, (x + 1) * TAMAÑO_CELDA, (y + 1) * TAMAÑO_CELDA);
 }
 
 // Dibujar el laberinto
@@ -51,13 +53,11 @@ void dibujarLab(const vector<vector<int>>& laberinto) {
     int size = laberinto.size();
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            if (laberinto[i][j] == 1) { // Dibujar muro
-                setfillstyle(SOLID_FILL, BLUE);
-                bar(j * TAMAÑO_CELDA, i * TAMAÑO_CELDA, (j + 1) * TAMAÑO_CELDA, (i + 1) * TAMAÑO_CELDA);
+            if (laberinto[i][j] == 1) {
+                dibujarCelda(j, i, BLUE);
             }
-            else { // Dibujar suelo
-                setfillstyle(SOLID_FILL, WHITE);
-                bar(j * TAMAÑO_CELDA, i * TAMAÑO_CELDA, (j + 1) * TAMAÑO_CELDA, (i + 1) * TAMAÑO_CELDA);
+            else {
+                dibujarCelda(j, i, WHITE);
             }
         }
     }
@@ -71,6 +71,13 @@ void DibujarPlayer(const Player& player) {
     int x2 = x1 + TAMAÑO_PLAYER;
     int y2 = y1 + TAMAÑO_PLAYER;
     bar(x1, y1, x2, y2);
+}
+
+// Actualizar el juego
+void actualizarJuego(Player& player, const vector<vector<int>>& lab, Player& prevPlayer) {
+    dibujarCelda(prevPlayer.x, prevPlayer.y, WHITE);
+    DibujarPlayer(player);
+    prevPlayer = player;
 }
 
 // Mover al jugador
@@ -94,49 +101,47 @@ void movePlayer(Player& player, const vector<vector<int>>& lab, char direccion) 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
 
-    vector<int> niveles = { 16, 20, 24 }; // Tamaño niveles
+    vector<int> niveles = { 16, 20, 24 };
     int nivelActual = 0;
-
-    const int TIEMPO_LIMITE = 60; 
+    const int TIEMPO_LIMITE = 60;
 
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
     while (nivelActual < static_cast<int>(niveles.size())) {
-        int tamañoLab = niveles[nivelActual]; // Tamaño del laberinto actual
+        int tamañoLab = niveles[nivelActual];
         vector<vector<int>> laber = iniciarLab(tamañoLab);
         conecInicFin(laber);
-
-        Player player = { 1, 1 }; // Posición inicial del jugador
+        //en vez de cleardevice solo dibuja al jugador otra vez
+        Player player = { 1, 1 };
+        Player prevPlayer = player;
         bool juegoActivo = true;
 
         auto inicioTiempo = steady_clock::now();
 
-        while (juegoActivo) {
-            cleardevice();
-            dibujarLab(laber);
-            DibujarPlayer(player);
+        dibujarLab(laber);
+        DibujarPlayer(player);
 
-            // Tiempo restante
+        while (juegoActivo) {
             auto tiempoActual = steady_clock::now();
             int tiempoRestante = TIEMPO_LIMITE - static_cast<int>(duration_cast<seconds>(tiempoActual - inicioTiempo).count());
 
-            // Mostrar tiempo
             setcolor(WHITE);
-            outtextxy(10, 10, const_cast<char*>(("Tiempo restante: " + to_string(max(0, tiempoRestante)) + " s").c_str()));
+            string textoTiempo = "Tiempo restante: " + to_string(max(0, tiempoRestante)) + " s";
+            char tiempoChar[100];
+            strcpy(tiempoChar, textoTiempo.c_str());
+            outtextxy(10, 10, tiempoChar);
 
-            // Verificar tiempo
             if (tiempoRestante <= 0) {
-                outtextxy(200, 200, const_cast<char*>("Has perdido :("));
+                outtextxy(200, 200, "Has perdido :(");
                 juegoActivo = false;
                 break;
             }
 
-            // Verificar victoria
             if (player.x == tamañoLab - 2 && player.y == tamañoLab - 2) {
-                outtextxy(200, 200, const_cast<char*>("¡Nivel completado!"));
-                delay(2000); // Esperar antes de pasar al siguiente nivel
-                nivelActual++; // Incrementar el nivel
+                outtextxy(200, 200, "¡Nivel completado!");
+                delay(2000);
+                nivelActual++;
                 juegoActivo = false;
                 break;
             }
@@ -144,13 +149,14 @@ int main() {
             if (kbhit()) {
                 char key = getch();
                 movePlayer(player, laber, key);
+                actualizarJuego(player, laber, prevPlayer);
             }
 
             delay(10);
         }
 
         if (nivelActual == static_cast<int>(niveles.size())) {
-            outtextxy(200, 200, const_cast<char*>("¡Felicitaciones! Has completado todos los niveles."));
+            outtextxy(200, 200, "¡Felicitaciones! Has completado todos los niveles.");
             break;
         }
     }
